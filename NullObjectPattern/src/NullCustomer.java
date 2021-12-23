5eba7abf6bc16c8d1de6101fd3a5935e3f22b35a
ihsansaif <96587855+ihsansaif@users.noreply.github.com>
2021-12-23 21:07:16 +0500

    Add files via upload
    NullObjectPattern
    First Commit

---
+++ NullObjectPattern/src/NullCustomer.java
@@ -0,0 +1,10 @@
+public class NullCustomer extends AbstractCustomer {
+
+   public String getName() {
+      return "Not Available in Customer Database";
+   }
+
+   public boolean isNil() {
+      return true;
+   }
+}
